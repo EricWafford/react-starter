9c6725f39e4491af6110a033a33ee0a0bb0c9bf3
Tomasz Mularczyk <tomasz.mularczyk@risendot.com>
2018-09-21 10:30:38 +0200

    fix createVendorChunk

--- config/paths.js
+++ config/paths.js
@@ -4,7 +4,6 @@ const root = path.resolve(__dirname, "../");
 const PATHS = {
   root,
   clientBundleEntry: path.join(root, "src/client.jsx"),
-  serverBundleEntry: path.join(root, "src/server/server.js"),
   htmlTemplate: path.join(root, "config/index.html"),
   mainOutputDirectory: path.join(root, "dist/")
 };
